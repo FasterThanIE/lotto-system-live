cd000ffa330272d620fb0f8c8e174e5a53415a0e
ITMentorstva <117232761+ITMentorstva@users.noreply.github.com>
2023-09-13 22:14:05 +0200

    * Navigation URL's

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Boomerionica</a>
+        <a class="navbar-brand" href="/">Boomerionica</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
                     @if(\Illuminate\Support\Facades\Auth::check())
@@ -15,9 +15,6 @@
                     @else
                         <a class="nav-link" href="/login">Login</a>
                     @endif
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                 </li>
             </ul>
         </div>
